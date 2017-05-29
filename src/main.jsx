86fdfd956a0239f3d0e2253efb3df65f55ba33c3
Adekunle Seun <adekunleseun001@gmail.com>
2017-05-29 18:34:56 +0100

    import login.jsx and scss styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,17 @@
-let message = "hello from seun";
-alert(message);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import Login from './components/Login.jsx';
+import '../public/styles/custom.scss';
+import {
+	BrowserRouter as Router,
+	Route,
+	NavLink
+} from 'react-router-dom';
+
+ReactDOM.render(
+	<Router>
+			<Route exact path='/' component={Login}/>
+	</Router>,
+	document.getElementById('app')
+	);
