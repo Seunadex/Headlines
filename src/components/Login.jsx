b3d18237553c614d37975874a9cef7dec7ef941b
Adekunle Seun <adekunleseun001@gmail.com>
2017-05-29 18:33:30 +0100

    Add code for google login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
+import createHistory from 'history/createBrowserHistory';
+import User from '../model/User';
+import Footer from './layout/Footer.jsx'
 
-const Login = () => {
-	/**
-		* function for responseSuccess
-		* saves user profile to local storage
-		* reloads page
-		*/
+const history = createHistory({
+	forceRefresh: true,
+});
 
-		const responseSuccess = (googleUser) => {
-			const userProfile = googleUser.getBasicProfile();
-			localStorage.setItem('User', JSON.stringify({
-				iD: userProfile.getId(),
-				name: userProfile.getName(),
-				email: userProfile.getEmail()
-			}));
-			location.reload();
+
+class Login extends Component {
+	componentWillMount() {
+		if (User.isLogin) {
+			history.push('/');
+		}
+	}
+
+	render() {
+		const responseGoogle = (response) => {
+			User.login(response);
+			window.location.reload();
 		};
-
-		const id = process.env.ID;
-
-		/**
-			* Function for responseFailure
-			* Logs out error
-			*/
-		const responseFailure = (response) => {
-			console.log(response);
-		};
-
 		return (
-			)
+			<div>
+				<div className='loginbody'>
+					<div className='container-fluid text-center'>
+						<h1>headlines</h1>
+						<p className='login-para'>View news headlines as it comes out</p>
+						<p className='login-para'>Stay up to date with news around the world</p>
+	
+						<GoogleLogin
+							className='login'
+							clientId='869248524764-gq40n0trb23md1r6op4b2v23qgdb6bj6.apps.googleusercontent.com'
+							buttonText='Login'
+							uxMode='popup'
+							onSuccess={responseGoogle}
+							onfailure={responseGoogle}
+						>
+						<span className='login-img'><img src='../img/google.png' alt='google login' /></span>
+						</GoogleLogin>
+					</div>
+				</div>
+					<div><Footer /></div>
+			</div>
+		);
+
+	}
 }
+
+export default Login;
